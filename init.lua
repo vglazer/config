96830457cb49a52689d641bedaae8dd50aa487fc
Victor Glazer <victor.glazer@gmail.com>
2021-12-25 14:40:20 -0500

    Added Hammerspoon config

---
+++ init.lua
@@ -0,0 +1,73 @@
+-- Snap active window to left half of the screen
+hs.hotkey.bind({"cmd", "shift"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Snap active window to right half of the screen
+hs.hotkey.bind({"cmd", "shift"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Snap active window to the top half of the screen
+hs.hotkey.bind({"cmd", "shift"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- Snap active window to the bottom half of the screen
+hs.hotkey.bind({"cmd", "shift"}, "Down", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- Maximize active window
+hs.hotkey.bind({"cmd", "shift", "alt"}, "Up", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Automatically reload configration whenever it is modified
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
